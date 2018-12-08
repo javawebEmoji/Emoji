8a7bce42e41c13195cc35f609d1ab5f2cba19edd
jc1144096387 <1144096387@qq.com>
2018-12-08 14:31:53 +0800

    merge

--- user.sql
+++ user.sql
@@ -1,41 +1,39 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 80013
-Source Host           : localhost:3306
-Source Database       : emoji
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : emoji
 
-Target Server Type    : MYSQL
-Target Server Version : 80013
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
 
-Date: 2018-12-04 19:48:26
+ Date: 08/12/2018 14:19:03
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `user`
+-- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `userid` int(4) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(16) NOT NULL,
+CREATE TABLE `user`  (
+  `userid` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `question` varchar(50) NOT NULL,
-  `answer` varchar(50) NOT NULL,
-  `email` varchar(20) DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
-  `hobby` varchar(50) DEFAULT NULL,
-  `sex` varchar(10) DEFAULT NULL,
-  `createtime` datetime DEFAULT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `question` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `answer` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `birthday` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `hobby` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sex` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createtime` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`userid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', 'root', '123', '13777841582', 'java', 'web', null, null, null, null, null);
-INSERT INTO `user` VALUES ('6', 'root', '12', '13777841581', 'java', 'web', null, null, null, null, '2018-12-04 16:41:25');
+SET FOREIGN_KEY_CHECKS = 1;
