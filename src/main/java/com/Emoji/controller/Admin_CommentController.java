1b0da01434ea8f4129c51faca4f70c859125bb63
jc1144096387 <1144096387@qq.com>
2018-12-17 20:00:07 +0800

    评论初步分页

--- src/main/java/com/Emoji/controller/Admin_CommentController.java
+++ src/main/java/com/Emoji/controller/Admin_CommentController.java
@@ -4,6 +4,8 @@ import com.Emoji.entity.Comment;
 import com.Emoji.entity.User;
 import com.Emoji.service.CommentService;
 import com.Emoji.service.UserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sun.xml.internal.ws.spi.db.DatabindingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletInputStream;
@@ -32,15 +35,24 @@ public class Admin_CommentController {
     private ArrayList<Comment> comments = null;
 
     @RequestMapping("admin/comment")
-    public String displayComments(Model model){
+    public String displayComments(@RequestParam(value="pn",defaultValue="1")Integer pn, Model model){
+        //从第一条开始 每页查询五条数据
+        PageHelper.startPage(pn, 5);
 
         if(comments == null){
             comments = commentService.selectAll();
+            //将评论信息放入PageInfo对象里
+            PageInfo page = new PageInfo(comments,5);
+            model.addAttribute("pageInfo", page);
+
             model.addAttribute("comments",comments);
+            comments = null;
         }else{
+            PageInfo page = new PageInfo(comments,5);
+            model.addAttribute("pageInfo", page);
+
             model.addAttribute("comments",comments);
         }
-        comments = null;
         return "admin/comment";
     }
 
@@ -52,8 +64,8 @@ public class Admin_CommentController {
     }
 
     @RequestMapping(value = "admin/comment/search")
-    public String searchComments(String username, String content,Model model){
-
+    public String searchComments(@RequestParam(value="pn",defaultValue="1")Integer pn,String username, String content,Model model){
+        PageHelper.startPage(pn, 5);
         System.out.println(username);
         System.out.println(content);
 //        System.out.println(startTime);
@@ -64,6 +76,8 @@ public class Admin_CommentController {
         comments = commentService.selectByCondition(params);
         System.out.println(comments.toString());
 
+        PageInfo page = new PageInfo(comments,5);
+        model.addAttribute("pageInfo", page);
         return "redirect:/admin/comment";
     }
 }
